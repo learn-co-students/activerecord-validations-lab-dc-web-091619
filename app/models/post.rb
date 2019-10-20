4180037595b949d6c502f359fb408df6a807cb79
heezhan <hhan1194@gmail.com>
2019-10-19 22:25:45 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,4 +21,3 @@ class Post < ActiveRecord::Base
     end 
 end
 
-# Finally, add a custom validator to Post that ensures the title is sufficiently clickbait-y. If the title does not contain "Won't Believe", "Secret", "Top [number]", or "Guess", the validator should add a validation.
